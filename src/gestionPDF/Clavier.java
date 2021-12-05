6b696787acdf09605d44bef0ace424d5b8f897a9
Pierre Lestringuez <pierre.lestringuez@iut-rodez.fr>
2021-12-05 15:10:12 +0100

    Fonctionnalité Souris scroll 3

--- src/gestionPDF/Clavier.java
+++ src/gestionPDF/Clavier.java
@@ -1,44 +1,32 @@
 package gestionPDF;
 
 import javax.swing.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+import java.awt.event.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
-import static gestionPDF.createPagePDF.nombrePage;
-import static gestionPDF.createPagePDF.tailleEspace;
+public class Clavier implements KeyListener, MouseWheelListener, MouseMotionListener, MouseListener {
 
-public class Clavier implements KeyListener, MouseWheelListener {
-    private JFrame frame;
-    private int gestionTab;
-    private JButton pageSuivante;
-    private JButton pagePrecedente;
-    private JTextField choixPage;
-    private JLabel nombreOfPage;
-    private JPanel panel;
-    private JScrollPane scrollPaneFrame;
-    configComposants.Counter c;
-    int height;
-    configComposants.NbScroll nbScroll;
-    configComposants.Tab tab;
+    private ContainerPDF containerPDF;
+    private ButtonPanel buttonPanel;
+    private FenetreApp fenetreApp;
+    private int y;
+    public Clavier() {
+        y=0;
+    }
 
-    public Clavier (JButton pageSuivante, JButton pagePrecedente, JTextField choixPage, JLabel nombreOfPage,
-                    JPanel panel1, configComposants.Counter c, int height, JFrame frame, JScrollPane scrollPaneFrame,
-                    configComposants.NbScroll nbScroll, configComposants.Tab tab) {
-        this.pagePrecedente = pagePrecedente;
-        this.pageSuivante = pageSuivante;
-        this.gestionTab = 0;
-        this.choixPage = choixPage;
-        this.nombreOfPage = nombreOfPage;
-        this.frame = frame;
-        this.panel = panel1;
-        this.c = c;
-        this.height = height;
-        this.scrollPaneFrame = scrollPaneFrame;
-        this.nbScroll = nbScroll;
-        this.tab = tab;
+    public void setContainerPDF(ContainerPDF containerPDF) {
+        this.containerPDF = containerPDF;
 
+    }
+
+    public void setButtonPanel(ButtonPanel buttonPanel) {
+        this.buttonPanel = buttonPanel;
+    }
+
+    public void setFenetreApp(FenetreApp fenetreApp) {
+        this.fenetreApp = fenetreApp;
     }
 
     /**
@@ -62,52 +50,64 @@ public class Clavier implements KeyListener, MouseWheelListener {
      */
     @Override
     public void keyPressed(KeyEvent e) {
-        int y;
-        int code = e.getKeyCode();
-        switch (code) {
-            case (KeyEvent.VK_RIGHT):
-                panel.requestFocus();
-                if (c.getValue() != nombrePage-1) {
-                    c.increment();
-                    nbScroll.setValue(c.getValue()*10);
-                    y = (height+tailleEspace) * (c.getValue());
-                    scrollPaneFrame.getVerticalScrollBar().setValue(y);
-                    choixPage.setText(String.valueOf(1+c.getValue()));
-                }
-                break;
-            case (KeyEvent.VK_LEFT):
-                panel.requestFocus();
-                if (c.getValue() != 0) {
-                    c.decrease();
-                    nbScroll.setValue(c.getValue()*10);
-                     y = (height+tailleEspace) * (c.getValue());
-                    scrollPaneFrame.getVerticalScrollBar().setValue(y);
-                    choixPage.setText(String.valueOf(1+c.getValue()));
-                }
-                break;
-            case (KeyEvent.VK_DOWN) :
-                descendre();
-                break;
-            case (KeyEvent.VK_UP) :
-                monter();
-                break;
-            case (KeyEvent.VK_A):
-                if (tab.getValue() > 3) {
-                    tab.decrease();
-                } else {
-                    tab.increment();
-                }
-                switch (tab.getValue()) {
-                    case 1 : pageSuivante.requestFocus();
-                    break;
-                    case 2 : pagePrecedente.requestFocus();
-                    break;
-                    case 3 : choixPage.requestFocus();
-                    break;
-                }
-          }
-        }
 
+        int code = e.getKeyCode();
+        if (buttonPanel.nombreDePage.hasFocus()) {
+            switch (code) {
+                case (KeyEvent.VK_ESCAPE):
+                    fenetreApp.mainWindow.requestFocus();
+                    break;
+                case (KeyEvent.VK_ENTER):
+                    if (buttonPanel.choixPage.hasFocus()) {
+                        fenetreApp.mainWindow.requestFocus();
+                    }
+                    break;
+            }
+        } else {
+            switch (code) {
+                case (KeyEvent.VK_RIGHT):
+                    containerPDF.documentPDF.requestFocus();
+                    pageSuivante();
+                    break;
+                case (KeyEvent.VK_LEFT):
+                    containerPDF.documentPDF.requestFocus();
+                    pagePrecedente();
+                    break;
+                case (KeyEvent.VK_DOWN):
+                    descendre();
+                    break;
+                case (KeyEvent.VK_UP):
+                    monter();
+                    break;
+                case (KeyEvent.VK_R):
+                    buttonPanel.choixPage.requestFocus();
+                    break;
+                case (KeyEvent.VK_A) :
+                    ArrayList<JLabel> pdf = new ArrayList<JLabel>();
+
+            }
+        }
+    }
+
+    private void pagePrecedente() {
+        System.out.println(buttonPanel.c.getValue());
+        if (buttonPanel.c.getValue() != 0) {
+            buttonPanel.c.decrease();
+            y = (containerPDF.height + buttonPanel.tailleEspace) * (buttonPanel.c.getValue());
+            containerPDF.scrollPaneContainer.getVerticalScrollBar().setValue(y);
+            buttonPanel.choixPage.setText(String.valueOf(1 + buttonPanel.c.getValue()));
+        }
+    }
+
+    private void pageSuivante() {
+        System.out.println(buttonPanel.c.getValue());
+        if (buttonPanel.c.getValue() != containerPDF.nombrePage - 1) {
+            buttonPanel.c.increment();
+            y = (containerPDF.height + buttonPanel.tailleEspace) * (buttonPanel.c.getValue());
+            containerPDF.scrollPaneContainer.getVerticalScrollBar().setValue(y);
+            buttonPanel.choixPage.setText(String.valueOf(1 + buttonPanel.c.getValue()));
+        }
+    }
 
     /**
      * Invoked when a key has been released.
@@ -121,37 +121,132 @@ public class Clavier implements KeyListener, MouseWheelListener {
 
     }
 
+    /**
+     * Invoked when the mouse wheel is rotated.
+     *
+     * @param e the event to be processed
+     * @see MouseWheelEvent
+     */
+    @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
-        int notches = e.getWheelRotation();
+        double notches = e.getPreciseWheelRotation();
+        System.out.println(notches);
         if (notches < 0) {
-            System.out.println(notches);
             monter();
         } else {
-            System.out.println(notches);
             descendre();
         }
     }
 
+
+
     private void descendre() {
-        int y = scrollPaneFrame.getVerticalScrollBar().getValue();
-        scrollPaneFrame.getVerticalScrollBar().setValue(y+50);
+        int y = containerPDF.scrollPaneContainer.getVerticalScrollBar().getValue();
+        containerPDF.scrollPaneContainer.getVerticalScrollBar().setValue(y+50);
 
-        if (c.getValue() != nombrePage-1) {
-            c.setValue(scrollPaneFrame.getVerticalScrollBar().getValue()/height);
+        if ( buttonPanel.c.getValue() != containerPDF.nombrePage-1) {
+            buttonPanel.c.setValue( containerPDF.scrollPaneContainer.getVerticalScrollBar().getValue()/containerPDF.height);
 
-            choixPage.setText(String.valueOf(1 + c.getValue()));
+            buttonPanel.choixPage.setText(String.valueOf(1 + buttonPanel.c.getValue()));
         }
     }
 
     private void monter() {
+        int y = containerPDF.scrollPaneContainer.getVerticalScrollBar().getValue();
+        containerPDF.scrollPaneContainer.getVerticalScrollBar().setValue(y-50);
+        if ( buttonPanel.c.getValue() != 0) {
+            buttonPanel.c.setValue( containerPDF.scrollPaneContainer.getVerticalScrollBar().getValue()/containerPDF.height);
 
-
-        int y = scrollPaneFrame.getVerticalScrollBar().getValue();
-        scrollPaneFrame.getVerticalScrollBar().setValue(y-50);
-        if (c.getValue() != 0) {
-            c.setValue(scrollPaneFrame.getVerticalScrollBar().getValue()/height);
-
-            choixPage.setText(String.valueOf(1 + c.getValue()));
+            buttonPanel.choixPage.setText(String.valueOf(1 + buttonPanel.c.getValue()));
         }
+    }
+
+
+
+
+    /**
+     * Invoked when a mouse button is pressed on a component and then
+     * dragged.  {@code MOUSE_DRAGGED} events will continue to be
+     * delivered to the component where the drag originated until the
+     * mouse button is released (regardless of whether the mouse position
+     * is within the bounds of the component).
+     * <p>
+     * Due to platform-dependent Drag&amp;Drop implementations,
+     * {@code MOUSE_DRAGGED} events may not be delivered during a native
+     * Drag&amp;Drop operation.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        y = containerPDF.scrollPaneContainer.getVerticalScrollBar().getValue();
+        System.out.println(y);
+        buttonPanel.c.setValue(containerPDF.scrollPaneContainer.getVerticalScrollBar().getValue()/containerPDF.height);
+        buttonPanel.choixPage.setText(String.valueOf(1 + buttonPanel.c.getValue()));
+    }
+
+    /**
+     * Invoked when the mouse cursor has been moved onto a component
+     * but no buttons have been pushed.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when the mouse button has been clicked (pressed
+     * and released) on a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        y = containerPDF.scrollPaneContainer.getVerticalScrollBar().getValue();
+        System.out.println(y);
+        buttonPanel.c.setValue(containerPDF.scrollPaneContainer.getVerticalScrollBar().getValue()/containerPDF.height);
+        buttonPanel.choixPage.setText(String.valueOf(1 + buttonPanel.c.getValue()));
+    }
+
+    /**
+     * Invoked when a mouse button has been pressed on a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when a mouse button has been released on a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when the mouse enters a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when the mouse exits a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
